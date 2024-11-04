9af6c7f2c20dc64a8cb2577eafb44d7e58b0486b
m-anass <manas8269@gmail.com>
2024-11-04 17:32:47 +0200

    Add Fluent Validation

--- Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
 using Restaurants.Application.Restuarants.Services;
 using System;
 using System.Collections.Generic;
@@ -12,9 +14,12 @@ namespace Restaurants.Application.Extensions
     {
         public static void AddApplication(this IServiceCollection services)
         {
+            var appAssembly = typeof(ServiceCollectionExtensions).Assembly;
             services.AddScoped<IRestaurantsService, RestaurantsService>();
 
-            services.AddAutoMapper(typeof(ServiceCollectionExtensions).Assembly);
+            services.AddAutoMapper(appAssembly);
+            services.AddValidatorsFromAssembly(appAssembly)
+                .AddFluentValidationAutoValidation();
         }
     }
 }
