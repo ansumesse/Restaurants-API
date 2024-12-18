1093f029acdd49c56f797186aa7cbb69117204fb
ansumesse <manas8269@gmail.com>
2024-12-18 18:32:08 +0200

    Pagination Request Validator

---
+++ Restaurants.Application/Restuarants/Validators/GetAllRestaurantsValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Restaurants.Application.Restuarants.Queries.GetAllRestaurants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Application.Restuarants.Validators
+{
+    public class GetAllRestaurantsValidator : AbstractValidator<GetAllRestaurantsQuery>
+    {
+        private readonly int[] allowedPageSizes = { 5, 10, 15, 30 };
+        public GetAllRestaurantsValidator()
+        {
+            RuleFor(r => r.PageSize)
+                .Must(p => allowedPageSizes.Contains(p))
+                .WithMessage($"Page size must be in [{string.Join(",", allowedPageSizes)}]");
+
+            RuleFor(r => r.PageNumber)
+                .GreaterThanOrEqualTo(1);
+        }
+    }
+}
