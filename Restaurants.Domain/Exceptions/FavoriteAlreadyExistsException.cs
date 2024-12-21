4985978c509e31c352fcf51b4b5af766e77cf1ad
ansumesse <manas8269@gmail.com>
2024-12-21 17:59:53 +0200

    Add Favorite  Exception

---
+++ Restaurants.Domain/Exceptions/FavoriteAlreadyExistsException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Domain.Exceptions
+{
+        public class FavoriteAlreadyExistsException : Exception
+        {
+            public FavoriteAlreadyExistsException(string resourceType)
+                : base($"You have already favorited this {resourceType}.")
+            {
+            }
+        }
+}
