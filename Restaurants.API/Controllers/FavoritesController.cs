66e5de89211003ec4808af764fe60774d213378c
ansumesse <manas8269@gmail.com>
2024-12-21 14:15:47 +0200

    Add Favorite and Unfavorite Dish Endpoints

--- Restaurants.API/Controllers/FavoritesController.cs
+++ Restaurants.API/Controllers/FavoritesController.cs
@@ -1,7 +1,9 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Restaurants.Application.Restuarants.Commands.FavoriteDish;
 using Restaurants.Application.Restuarants.Commands.FavoriteRestaurant;
+using Restaurants.Application.Restuarants.Commands.UnFavoriteDish;
 using Restaurants.Application.Restuarants.Commands.UnFavoriteRestaurant;
 
 namespace Restaurants.API.Controllers
@@ -21,6 +23,20 @@ namespace Restaurants.API.Controllers
         public async Task<IActionResult> UnFavoriteRestaurant(int id)
         {
             await mediator.Send(new UnFavoriteRestaurantCommand(id));
+            return NoContent();
+        }
+
+        [HttpPost("Dish/{dishId}")]
+        public async Task<IActionResult> FavoriteDish(int id, int dishId)
+        {
+            await mediator.Send(new FavoriteDishCommand(id, dishId));
+            return NoContent();
+        }
+
+        [HttpDelete("Dish/{dishId}")]
+        public async Task<IActionResult> UnFavoriteDish(int id, int dishId)
+        {
+            await mediator.Send(new UnFavoriteDishCommand(id, dishId));
             return NoContent();
         }
 
