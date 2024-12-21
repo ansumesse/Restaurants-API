12780c06160534b9f6305034fc232a300e416010
ansumesse <manas8269@gmail.com>
2024-12-21 17:29:43 +0200

    Add Get AllFavoriteRestaurants

--- Restaurants.API/Controllers/FavoritesController.cs
+++ Restaurants.API/Controllers/FavoritesController.cs
@@ -5,35 +5,43 @@ using Restaurants.Application.Restuarants.Commands.FavoriteDish;
 using Restaurants.Application.Restuarants.Commands.FavoriteRestaurant;
 using Restaurants.Application.Restuarants.Commands.UnFavoriteDish;
 using Restaurants.Application.Restuarants.Commands.UnFavoriteRestaurant;
+using Restaurants.Application.Restuarants.Queries.GetFavoriteRestaurants;
 
 namespace Restaurants.API.Controllers
 {
-    [Route("api/[controller]/Restaurant/{id}")]
+    [Route("api/[controller]")]
     [ApiController]
     public class FavoritesController(IMediator mediator) : ControllerBase
     {
-        [HttpPost]
+        [HttpGet("Restuarant")]
+        public async Task<IActionResult> GetFavoriteRestaurants()
+        {
+            var restaurants = await mediator.Send(new GetFavoriteRestaurantsQuery());
+            return Ok(restaurants);
+        }
+
+        [HttpPost("Restaurant/{id}")]
         public async Task<IActionResult> FavoriteRestaurant(int id)
         {
             await mediator.Send(new FavoriteRestaurantCommand(id));
             return NoContent();
         }
 
-        [HttpDelete]
+        [HttpDelete("Restaurant/{id}")]
         public async Task<IActionResult> UnFavoriteRestaurant(int id)
         {
             await mediator.Send(new UnFavoriteRestaurantCommand(id));
             return NoContent();
         }
 
-        [HttpPost("Dish/{dishId}")]
+        [HttpPost("Restaurant/{id}/Dish/{dishId}")]
         public async Task<IActionResult> FavoriteDish(int id, int dishId)
         {
             await mediator.Send(new FavoriteDishCommand(id, dishId));
             return NoContent();
         }
 
-        [HttpDelete("Dish/{dishId}")]
+        [HttpDelete("Restaurant/{id}/Dish/{dishId}")]
         public async Task<IActionResult> UnFavoriteDish(int id, int dishId)
         {
             await mediator.Send(new UnFavoriteDishCommand(id, dishId));
---
+++ Restaurants.Application/Restuarants/Queries/GetFavoriteRestaurants/GetFavoriteRestaurantsQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Restaurants.Application.Common;
+using Restaurants.Application.Restuarants.Dtos;
+using Restaurants.Application.Users;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Exceptions;
+using Restaurants.Domain.Repositories.Restaurants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Application.Restuarants.Queries.GetFavoriteRestaurants
+{
+    public class GetFavoriteRestaurantsQueryHandler(ILogger<GetFavoriteRestaurantsQueryHandler> logger,
+        IRestaurantsRepository restaurantsRepository,
+        IUserContext userContext,
+        IMapper mapper) : IRequestHandler<GetFavoriteRestaurantsQuery, IEnumerable<RestaurantDto>>
+    {
+        public async Task<IEnumerable<RestaurantDto>> Handle(GetFavoriteRestaurantsQuery request, CancellationToken cancellationToken)
+        {
+            var user = userContext.GetCurrentUser();
+            logger.LogInformation(@"Getting User #{UserId} Favorite restaurants", user!.Id);
+
+            var favRestaurants = await restaurantsRepository.GetFavoriteRestaurants(user.Id);
+            if (!favRestaurants.Any())
+                throw new NotFoundException(nameof(FavoriteRestaurant), user.Id);
+
+            var restaurantIds = favRestaurants.Select(f => f.RestaurantId);
+
+            var restaurants = await restaurantsRepository.GetRestaurantsByIdsAsync(restaurantIds);
+
+            return restaurants.Select(restaurant => mapper.Map<RestaurantDto>(restaurant));
+        }
+    }
+}
