6cad03619145a0b772acc83ae2dcc1144ebb619e
ansumesse <manas8269@gmail.com>
2024-12-21 14:13:57 +0200

    Add Unfavorite Dish command

---
+++ Restaurants.Application/Restuarants/Commands/UnFavoriteDish/UnFavoriteDishCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Application.Restuarants.Commands.UnFavoriteDish
+{
+    public class UnFavoriteDishCommand(int restaurantId, int dishId) : IRequest
+    {
+        public int RestaurantId { get; } = restaurantId;
+        public int DishId { get; } = dishId;
+    }
+}
