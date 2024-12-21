b0c89801e4386a4a45ba054e05b809e6ffd85719
ansumesse <manas8269@gmail.com>
2024-12-21 17:59:01 +0200

    Adjust NotFoundException

--- Restaurants.Domain/Exceptions/NotFoundException.cs
+++ Restaurants.Domain/Exceptions/NotFoundException.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Restaurants.Domain.Exceptions
 {
-    public class NotFoundException(string resourceType, string resourceId) : Exception($"{resourceType} with id: {resourceId} doesnot exist")
+    public class NotFoundException : Exception
     {
+        public NotFoundException(string resourceType, string resourceId)
+            : base($"{resourceType} with id: {resourceId} does not exist.")
+        {
+        }
     }
 }
