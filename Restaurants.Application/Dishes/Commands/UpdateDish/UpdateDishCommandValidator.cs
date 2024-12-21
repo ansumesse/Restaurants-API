d451d8b78d5babb9883796ee68dc7b84d6b29fd4
ansumesse <manas8269@gmail.com>
2024-12-21 19:56:44 +0200

    Add UpdateDishCommandValidator

---
+++ Restaurants.Application/Dishes/Commands/UpdateDish/UpdateDishCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Restaurants.Application.Dishes.Commands.UpdateDish
+{
+    public class UpdateDishCommandValidator : AbstractValidator<UpdateDishCommand>
+    {
+        public UpdateDishCommandValidator()
+        {
+            RuleFor(d => d.Price)
+               .GreaterThanOrEqualTo(0)
+               .WithMessage("Price Cannot be Negative value");
+
+            RuleFor(d => d.KiloCalories)
+                .GreaterThanOrEqualTo(0)
+                .WithMessage("KiloCalories Cannot be Negative value");
+        }
+    }
+}
