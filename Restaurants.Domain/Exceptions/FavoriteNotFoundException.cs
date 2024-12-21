873a7abe603e699c0b07d9b2d7796ed2dd3d7611
ansumesse <manas8269@gmail.com>
2024-12-21 18:12:01 +0200

    Add FavoriteNotFoundException

---
+++ Restaurants.Domain/Exceptions/FavoriteNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Domain.Exceptions
+{
+    public class FavoriteNotFoundException : Exception
+    {
+        public FavoriteNotFoundException(string resourceType, string resourceId)
+            : base($"The favorite {resourceType} with id: {resourceId} was not found.")
+        {
+        }
+    }
+}
