e7b7e863f79393e55777607f0fe2ab4fd248803e
ansumesse <manas8269@gmail.com>
2024-12-27 19:54:34 +0200

    Add GetById With Authorization Tests

--- Restaurants.APITests/Controllers/RestaurantControllerTests.cs
+++ Restaurants.APITests/Controllers/RestaurantControllerTests.cs
@@ -1,18 +1,37 @@
 ﻿using Xunit;
-using Restaurants.API.Controllers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
 using FluentAssertions;
+using Moq;
+using Restaurants.Domain.Repositories.Restaurants;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Restaurants.APITests;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Restaurants.Application.Restuarants.Dtos;
+using Restaurants.Domain.Entities;
+using System.Net.Http.Json;
+using System.Net;
 
 namespace Restaurants.API.Controllers.Tests
 {
-    public class RestaurantControllerTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
+    public class RestaurantControllerTests : IClassFixture<WebApplicationFactory<Program>>
     {
-        private readonly WebApplicationFactory<Program> _factory = factory;
+        private readonly WebApplicationFactory<Program> _factory;
+        private readonly Mock<IRestaurantsRepository> _restaurantsRepositoryMock = new();
+        public RestaurantControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            _factory = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddSingleton<IPolicyEvaluator, FakepolicyEvaluator>();
+                    services.Replace(ServiceDescriptor.Scoped(typeof(IRestaurantsRepository),
+                                                _ => _restaurantsRepositoryMock.Object));
+                });
+            });
+        }
 
         [Fact()]
         public async Task GetAll_ForValidRequest_Returns200Ok()
@@ -24,7 +43,47 @@ namespace Restaurants.API.Controllers.Tests
             var result = await client.GetAsync("api/Restaurant");
 
             // Assert
-            result.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task GetById_ForNonExistingId_ShouldReturn404NotFound()
+        {
+            // arrange
+            var id = 1123;
+            _restaurantsRepositoryMock.Setup(m => m.GetRestaurantByIdAsync(id)).ReturnsAsync((Restaurant?)null);
+
+            var client = _factory.CreateClient();
+            // act
+            var response = await client.GetAsync($"/api/restaurant/{id}");
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task GetById_ForExistingId_ShouldReturn200Ok()
+        {
+            // arrange
+            var id = 99;
+            var restaurant = new Restaurant()
+            {
+                Id = id,
+                Name = "Test",
+                Description = "Test description"
+            };
+            _restaurantsRepositoryMock.Setup(m => m.GetRestaurantByIdAsync(id)).ReturnsAsync(restaurant);
+            var client = _factory.CreateClient();
+
+            // act
+            var response = await client.GetAsync($"/api/restaurant/{id}");
+            var restaurantDto = await response.Content.ReadFromJsonAsync<RestaurantDto>();
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            restaurantDto.Should().NotBeNull();
+            restaurantDto.Name.Should().Be("Test");
+            restaurantDto.Description.Should().Be("Test description");
         }
     }
 }
